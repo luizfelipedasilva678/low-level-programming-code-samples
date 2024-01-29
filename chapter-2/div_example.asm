38372f6773ec5e6634bebf20600da258ab21b450
Luiz Felipe <luizfelipedasilva678@gmail.com>
2024-01-28 22:07:51 -0300

    DIv example

---
+++ chapter-2/div_example.asm
@@ -0,0 +1,23 @@
+global _start
+
+section .data
+    divisor: db 1
+    result: dq 0
+
+section .text
+
+_start:
+    mov rax, 48
+    div qword [divisor]
+
+    mov [result], rax
+
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, result 
+    mov rdx, 1
+    syscall
+
+    mov rax, 60
+    xor rdi, rdi
+    syscall
