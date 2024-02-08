6cd9cb37e63605bbc310d72c38537b42c4489e65
Luiz Felipe <luizfelipedasilva678@gmail.com>
2024-02-07 23:48:03 -0300

    Using mmap

--- chapter-4/factorial.asm
+++ chapter-4/factorial.asm
@@ -1,8 +1,13 @@
+%define O_RDONLY 0
+%define PROT_READ 0x1
+%define MAP_PRIVATE 0x2
+
 
 global _start
 
 section .data
     number: db 10
+    fname: db 'input.txt', 0
 
 section .text
 string_length:
@@ -51,6 +56,26 @@ print_uint:
 
     ret
 
+parse_uint:
+    mov r8, 10
+    xor rax, rax
+    xor rcx, rcx
+.loop:
+    movzx r9, byte [rdi + rcx]
+    cmp r9b, '0'
+    jb .end
+    cmp r9b, '9'
+    ja .end
+    xor rdx, rdx
+    mul r8
+    and r9b, 0x0f
+    add rax, r9
+    inc rcx
+    jmp .loop
+    .end:
+    mov rdx, rcx
+    ret
+
 _end:
     mov rdi, rax
     call print_uint
@@ -60,8 +85,24 @@ _end:
     syscall
 
 _start:
-    mov rax, [number]
-    mov r9, [number]
+    mov rax, 2
+    mov rdi, fname
+    mov rsi, O_RDONLY
+    mov rdx, 0
+    syscall
+
+    mov r8, rax
+    mov rax, 9
+    mov rdi, 0
+    mov rsi, 4096
+    mov rdx, PROT_READ
+    mov r10, MAP_PRIVATE
+    mov r9, 0
+    syscall
+
+    mov rdi, rax
+    call parse_uint
+    mov r9, rax
     dec r9
 
     .loop:
