ac71a8dbc071e6c27aa9b0b8e261fd8f0655193b
Luiz Felipe <luizfelipedasilva678@gmail.com>
2024-02-08 18:54:45 -0300

    Sum of digits mmap

--- chapter-4/sum_of_digits.asm
+++ chapter-4/sum_of_digits.asm
@@ -1,10 +1,35 @@
+%define O_RDONLY 0
+%define PROT_READ 0x1
+%define MAP_PRIVATE 0x2
 
 global _start
 
 section .data
     number: db 123
+     fname: db 'input.txt', 0
 
 section .text
+
+parse_uint:
+    mov r8, 10
+    xor rax, rax
+    xor rcx, rcx
+.loop:
+    movzx r9, byte [rdi + rcx]
+    cmp r9b, '0'
+    jb .end
+    cmp r9b, '9'
+    ja .end
+    xor rdx, rdx
+    mul r8
+    and r9b, 0x0f
+    add rax, r9
+    inc rcx
+    jmp .loop
+    .end:
+    mov rdx, rcx
+    ret
+
 string_length:
     xor rax, rax
 
@@ -58,7 +83,24 @@ _end:
     syscall
 
 _start:
-    mov rax, [number]
+    mov rax, 2
+    mov rdi, fname
+    mov rsi, O_RDONLY
+    mov rdx, 0
+    syscall
+
+    mov r8, rax
+    mov rax, 9
+    mov rdi, 0
+    mov rsi, 4096
+    mov rdx, PROT_READ
+    mov r10, MAP_PRIVATE
+    mov r9, 0
+    syscall
+
+    mov rdi, rax
+    call parse_uint
+
     xor r8, r8
     mov r9, 10
 
