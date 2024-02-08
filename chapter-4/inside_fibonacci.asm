5fcd4702a4b7015f49471b87412b52f157dcd813
Luiz Felipe <luizfelipedasilva678@gmail.com>
2024-02-08 18:52:19 -0300

    Inside fibonacci mmap

--- chapter-4/inside_fibonacci.asm
+++ chapter-4/inside_fibonacci.asm
@@ -1,9 +1,35 @@
+%define O_RDONLY 0
+%define PROT_READ 0x1
+%define MAP_PRIVATE 0x2
+
+
 global _start
 
 section .data
     number: db 4
+    fname: db 'input.txt', 0
 
 section .text
+
+parse_uint:
+    mov r8, 10
+    xor rax, rax
+    xor rcx, rcx
+.loop:
+    movzx r9, byte [rdi + rcx]
+    cmp r9b, '0'
+    jb .end
+    cmp r9b, '9'
+    ja .end
+    xor rdx, rdx
+    mul r8
+    and r9b, 0x0f
+    add rax, r9
+    inc rcx
+    jmp .loop
+    .end:
+    mov rdx, rcx
+    ret
 
 found:
     mov rax, 60
@@ -16,16 +42,35 @@ _end:
     syscall
 
 _start:
+    mov rax, 2
+    mov rdi, fname
+    mov rsi, O_RDONLY
+    mov rdx, 0
+    syscall
+
+    mov r8, rax
+    mov rax, 9
+    mov rdi, 0
+    mov rsi, 4096
+    mov rdx, PROT_READ
+    mov r10, MAP_PRIVATE
+    mov r9, 0
+    syscall
+
+    mov rdi, rax
+    call parse_uint
+    mov rcx, rax
+
+    cmp rcx, 0
+    jz _end
+    cmp rcx, 0
+    jz _end
+    
+    dec rcx
+
     mov r9, 1
     xor r8, r8 
     xor rax, rax
-    mov rcx, [number]
-    dec rcx
-
-    cmp byte [number], 0
-    jz _end
-    cmp byte [number], 0
-    jz _end
 
     .loop:
         add r9, r8
@@ -35,9 +80,9 @@ _start:
         mov r8, r9
         pop r9
 
-        cmp byte [number], r8b
+        cmp rcx, r8
         je found
-        cmp byte [number], r9b
+        cmp rcx, r9
         je found
         cmp rax, rcx
         jg _end
