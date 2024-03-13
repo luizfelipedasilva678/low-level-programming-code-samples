a61686a40401a174a66bf149bd9c87c3ca47ed4d
Luiz Felipe <luizfelipedasilva678@gmail.com>
2024-03-12 21:04:58 -0300

    Prime checker

--- assignments/prime_checker/prime_checker.c
+++ assignments/prime_checker/prime_checker.c
@@ -1,36 +1,25 @@
 #include<stdio.h>
-
 #define TRUE 1
 #define FALSE 0
 
-int number_of_divisors(unsigned long n) {
-    size_t i;
-    int count = 0;
-
-    for (i = 1; i <= n; i++) {
-        if(n % i == 0) {
-            count++;
-        }
-    }
-
-    return count;
-
-}
 
 int is_prime(unsigned long n) {
-    if(n == 1 || n <= 0 || number_of_divisors(n) > 2) {
-        return FALSE;
+    size_t i;
+
+    if(n == 0UL || n == 1UL) return FALSE;
+
+    for (i = 1; i < n; i++) {
+        if (n % i == 0UL && i != 1UL && i != n) return FALSE;
     }
-    
+
     return TRUE;
 }
 
 int main(void) {
-    unsigned long n;
+    unsigned long n = 0UL;
 
-    while (! scanf("%lu", &n) );
+    while (!scanf("%lu", &n));
 
-    printf(is_prime(n) ? "yes \n" : "no \n");
-
+    printf(is_prime(n) ? "Prime\n" : "Not Prime\n");
     return 0;
 }
