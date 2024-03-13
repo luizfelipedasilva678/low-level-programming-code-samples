4cb1daacc1b72f28532dbacec102cd8eaeb7bdea
Luiz Felipe <40125808+luizfelipedasilva678@users.noreply.github.com>
2024-03-13 17:08:18 -0300

    Update c_parametric_polymorphism.c

--- chapter-9/c_parametric_polymorphism.c
+++ chapter-9/c_parametric_polymorphism.c
@@ -7,3 +7,21 @@
     T fst;\
     T snd;\
 };\
+bool pair_##T##_any(struct pair(T) pair, bool (*predicate)(T)) {\
+    return predicate(pair.fst) || predicate(pair.snd);\
+}
+
+#define any(T) pair_##T##_any
+
+DEFINE_PAIR(int)
+
+bool is_positive( int x ) { return x > 0; }
+
+int main(int argc, char ** argv) {
+    struct pair(int) obj;
+    obj.fst = 1;
+    obj.snd = -2;
+    printf("%d\n", any(int)(obj, is_positive));
+
+    return 0;
+}
